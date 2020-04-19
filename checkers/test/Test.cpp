ca2af3e91f222323b4947d8d414ad950a647ae38
Patrycja <patrycja.cieplicka@gmail.com>
2020-04-19 21:43:40 +0200

    add second test and small fixes

--- checkers/test/Test.cpp
+++ checkers/test/Test.cpp
@@ -10,9 +10,9 @@
 #include "../src/Pawn.hpp"
 
 
+BOOST_AUTO_TEST_SUITE(MovePiece)
 
-
-BOOST_AUTO_TEST_CASE( movePos ) //change Pawn to King
+BOOST_AUTO_TEST_CASE( changePawKing ) //change Pawn to King
 {   
 
     Board board = Board();
@@ -21,7 +21,7 @@ BOOST_AUTO_TEST_CASE( movePos ) //change Pawn to King
     user.addPiece(false, Position(1, 5),board);
     computer.addPiece(true, Position(2,6), board);
     computer.addPiece(false, Position(5,5), board);
-    std::cout << board << std::endl;
+
     auto moves = user.getValidMovePiece(board, 0);
 
     BOOST_CHECK(moves.size() == 2);
@@ -36,6 +36,39 @@ BOOST_AUTO_TEST_CASE( movePos ) //change Pawn to King
     Position rPos(6,4);
 
     BOOST_CHECK(newUs[0]->getPosition() == rPos);
-    BOOST_CHECK(board.getPieceName(rPos));
+    BOOST_CHECK(board.getPieceName(rPos) == WhiteKing);
 
 }
+
+BOOST_AUTO_TEST_CASE( pawn ) //check Pawn
+{   
+
+    Board board = Board();
+    Player user = Player(&board, true, true);
+    Player computer = Player(&board, false, false);
+    user.addPiece(false, Position(2, 2),board);
+    computer.addPiece(false, Position(3,3), board);
+    computer.addPiece(false, Position(1,1), board);
+    computer.addPiece(false, Position(5,3), board);
+    auto moves = user.getValidMovePiece(board, 0);
+
+    BOOST_CHECK(moves.size() == 2);
+
+    user.movePiece(board, computer, moves[1]);
+
+    std::vector<Piece*> newComp = computer.getPieces();
+    std::vector<Piece*> newUs = user.getPieces();
+
+    BOOST_CHECK(newComp.size() == 1 || newComp.size() == 2 );
+    BOOST_CHECK(newUs.size() == 1);
+
+    Position rPos(0,0);
+
+    BOOST_CHECK(newUs[0]->getPosition() == rPos);
+    BOOST_CHECK(board.getPieceName(rPos) == WhitePawn);
+
+}
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
