032a9cbde3e8ccc6d56910a367450da49d1164a1
Patrycja <patrycja.cieplicka@gmail.com>
2020-04-19 21:48:21 +0200

    add king test

--- checkers/test/Test.cpp
+++ checkers/test/Test.cpp
@@ -70,5 +70,34 @@ BOOST_AUTO_TEST_CASE( pawn ) //check Pawn
 }
 
 
+BOOST_AUTO_TEST_CASE( pawn ) //check King
+{   
+
+    Board board = Board();
+    Player user = Player(&board, true, true);
+    Player computer = Player(&board, false, false);
+    user.addPiece(true, Position(2, 2),board);
+    computer.addPiece(false, Position(3,3), board);
+    computer.addPiece(false, Position(1,1), board);
+    computer.addPiece(false, Position(5,3), board);
+    auto moves = user.getValidMovePiece(board, 0);
+
+    BOOST_CHECK(moves.size() == 2);
+
+    user.movePiece(board, computer, moves[1]);
+
+    std::vector<Piece*> newComp = computer.getPieces();
+    std::vector<Piece*> newUs = user.getPieces();
+
+    BOOST_CHECK(newComp.size() == 1 || newComp.size() == 2 );
+    BOOST_CHECK(newUs.size() == 1);
+
+    Position rPos(0,0);
+
+    BOOST_CHECK(newUs[0]->getPosition() == rPos);
+    BOOST_CHECK(board.getPieceName(rPos) == WhiteKing);
+
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
